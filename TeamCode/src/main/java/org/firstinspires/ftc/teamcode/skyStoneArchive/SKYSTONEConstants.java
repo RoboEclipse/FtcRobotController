a00894cfb26b6eb43e3357dd163405c60902a8ce
erichu100 <erichu100@outlook.com>
2020-10-04 14:18:15 -0700

    Added Roadrunner and Last Season Archive -Added OpenFTC, Dashboard and Roadrunner to Gradle -Copied over various important programs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skyStoneArchive/SKYSTONEConstants.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.skyStoneArchive;
+
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class SKYSTONEConstants {
+
+    //TicksPerRotation is 42.78 on testbot
+    private static double TICKS_PER_ROTATION = 103.6/3.7;
+    private static double GEAR_RATIO = 16;
+    private static double TICKS_PER_WHEEL_ROTATION = TICKS_PER_ROTATION*GEAR_RATIO;
+    static double TICKS_PER_INCH = TICKS_PER_WHEEL_ROTATION/(4* Math.PI);
+
+    //Speeds
+    static double maxSpeed = 57;
+
+    public static int stoneDiff = 3;
+
+    //Skystone Tele-op Stacking Claw Rotation Servo Constants
+    public static double right90 = 1; //90 degrees right from straight
+    public static double left90 = 0.39; //90 degrees left from straight
+    public static double straight = 0.75; //Straight
+    public static double oppositeSide = 0.00; //Make it straight but on the other side or back side
+    //Skystone Tele-op Stacking Grabber Claw
+    public static double tighten = 0.7;
+    public static double loosen = 0.25;
+    //Skystone Right Foundation Claw
+    public static double rDown = 0.72;
+    public static double rUp = 0.53;
+    //Skystone Left Foundation Claw
+    public static double lDown = 0.3; //At least .5
+    public static double lUp = 0.55;
+
+    public static double frontClawDown = 0.58;
+    public static double frontClawUp = 1;
+    //Capstone Servo
+    public static double cUp = 0.28;
+    public static double cDown = 0.6; //At least 0.55
+    //Front right claw
+    public static double frontLow = 0;
+    public static double frontPlace = 0.45;
+    public static double frontHigh = 1;
+    public static double frontGrab = 0.0;
+    public static double frontLoosen = 0.7;
+    public static double frDown = 0.53;
+    public static double frUp = 0.95;
+    public static double frSuperUp = 1;
+    //Front left claw
+    public static double flDown = 0.78;
+    public static double flUp = 0.3;
+    public static double flSuperUp = 0.28;
+    //Mode Switch Servo
+    public static double deliverMode = 0.6;
+    public static double stackingMode = 0.2;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skyStoneArchive/SKYSTONEDrivetrainClass.java
@@ -0,0 +1,89 @@
+package org.firstinspires.ftc.teamcode.skyStoneArchive;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.openftc.revextensions2.ExpansionHubEx;
+import org.openftc.revextensions2.ExpansionHubMotor;
+
+public class SKYSTONEDrivetrainClass {
+    DcMotor lb, lf, rb, rf;
+    ExpansionHubMotor lbBR, lfBR, rbBR, rfBR;
+    ExpansionHubEx expansionHub;
+    DistanceSensor leftDistance,backDistance, frontDistance;
+    SKYSTONEConfiguration skystoneNames = new SKYSTONEConfiguration();
+    // The IMU sensor object
+    BNO055IMU imu;
+    Orientation angles;
+    void initializeDriveTrain(HardwareMap hardwareMap, Telemetry telemetry){
+        lb = hardwareMap.dcMotor.get("backLeft");
+        lf = hardwareMap.dcMotor.get("frontLeft");
+        rb = hardwareMap.dcMotor.get("backRight");
+        rf = hardwareMap.dcMotor.get("frontRight");
+        leftDistance = hardwareMap.get(DistanceSensor.class, skystoneNames.leftDistance);
+        backDistance = hardwareMap.get(DistanceSensor.class, skystoneNames.backDistance);
+        frontDistance = hardwareMap.get(DistanceSensor.class, skystoneNames.frontDistance);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        /* Bulk Read Styff
+        expansionHub = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
+        lbBR = (ExpansionHubMotor) hardwareMap.dcMotor.get("backLeft");
+        lfBR = (ExpansionHubMotor) hardwareMap.dcMotor.get("frontLeft");
+        rbBR = (ExpansionHubMotor) hardwareMap.dcMotor.get("backRight");
+        rfBR = (ExpansionHubMotor) hardwareMap.dcMotor.get("frontRight");
+        */
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lb.setTargetPosition(300);
+        lb.setPower(1);
+
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    void runMotors (double leftPower, double rightPower){
+        lb.setPower(leftPower);
+        lf.setPower(leftPower);
+        rb.setPower(rightPower);
+        rf.setPower(rightPower);
+    }
+    double getBackDistance(){
+        return backDistance.getDistance(DistanceUnit.INCH);
+    }
+    double getFrontDistance() {
+        return frontDistance.getDistance(DistanceUnit.INCH);
+    }
+    double getLeftDistance() {
+        return leftDistance.getDistance(DistanceUnit.INCH);
+    }
+}
