7afe6e62cdeea0c10191a143934d6b09e1246f43
erichu100 <erichu100@outlook.com>
2020-10-07 20:37:36 -0700

    Added new package for baseBots, added roadrunner implementation test code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseBot/Configuration.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.baseBot;
+
+class Configuration {
+    //Drive Motors
+    final String backLeftMotor = "backLeft";
+    final String frontLeftMotor = "frontLeft";
+    final String backRightMotor = "backRight";
+    final String frontRightMotor = "frontRight";
+
+    //Attachment motors
+    final String slidingMotor = "slidingMotor";
+    final String elevator = "elevatorMotor";
+    final String rightCollectorMotor = "rightCollector";
+    final String leftCollectorMotor = "leftCollector";
+    //Autonomous Servos
+    final String modeSwitch = "modeSwitch";
+    final String leftFoundationServo = "leftFoundationServo";
+    final String rightFoundationServo = "rightFoundationServo";
+    //Previously frontBase
+    final String frontBase = "frontBase";
+    //Previously frontGrabber
+    final String frontGrabber = "frontGrabber";
+    //Previously backBase
+    final String backBase = "backBase";
+    //Previously backGrabber
+    final String backGrabber = "backGrabber";
+
+    //Tele-Op Servos
+    final String collectorServo = "clawServo";
+    final String rotationServo = "rotationServo";
+    final String cappingServo = "capStoneServo";
+
+    //Distance Sensors
+    final String frontDistance = "frontDistance";
+    final String backDistance = "backDistance";
+    //Previously elevatorHeight
+    final String leftDistance = "leftDistance";
+
+    //Color Sensors
+    final String frontColor = "frontColor";
+    final String backColor = "backColor";
+
+    //Limit Switch
+    final String limitSwitch = "limitSwitch";
+}
