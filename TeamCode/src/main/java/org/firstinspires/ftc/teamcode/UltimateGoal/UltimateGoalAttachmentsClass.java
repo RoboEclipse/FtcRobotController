aa5ff2c717854c3c136f7d3b049b84481851b830
erich <erichu100@outlook.com>
2020-11-22 19:58:08 -0800

    Added old autonomous structure and started new one.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/UltimateGoalAttachmentsClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/UltimateGoalAttachmentsClass.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.baseBot.DrivetrainClass;
 import org.firstinspires.ftc.teamcode.skyStoneArchive.SKYSTONEConstants;
 
-public class UltimateGoalClass extends DrivetrainClass {
+public class UltimateGoalAttachmentsClass extends DrivetrainClass {
     private Telemetry telemetry;
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor flywheelMotor = null;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseBot/BaseBotAutonomousClass.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.baseBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+abstract class BaseBotAutonomousClass extends LinearOpMode {
+    //GetAngle
+}
