8e37253c209cc1abc2ceae33504984359a26621c
ericzhou16 <ericsuper@outlook.com>
2020-11-29 20:56:19 -0800

    Cleaned up files and disabled some old teleOp files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseBot/ConceptScanServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseBot/ConceptScanServoTest.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.baseBot;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -48,7 +49,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 @TeleOp(name = "Concept: Scan Servo", group = "Concept")
-//@Disabled
+@Disabled
 public class ConceptScanServoTest extends LinearOpMode {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
