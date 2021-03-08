218655da8ed9f26018c9ed941f6b70ea198edf54
ericzhou16 <ericsuper@outlook.com>
2021-03-07 20:25:48 -0800

    DetectionTest, Teleop updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Constants.java
@@ -10,9 +10,29 @@ public class Constants {
     //Speeds
     static double maxSpeed = 57;
 
+
+
+    //rest position(beginning setup)
+    static int RESTPOS = 0;
+
+    //grabber has to be angled during game to prevent from hitting elevator
+    static int RUNPOS = 200;
+
+    //lift wobble goal up to move over border of field
+    static int VERTICALPOS = 700;
+
+    //horizontal position where wobble goal being placed outside of field
+    static int HORIZONTALPOS = 1360;
+
+    //angled down to grab wobble goal
+    static int GRABPOS = 1555;
+
+
+
     //Autonomous constants
     static int wobbleTop = 0;
     static int wobbleBottom = 0;
+
 
     //Tele-op constants
     static double wobbleHoldingPower = 0;
@@ -23,16 +43,16 @@ public class Constants {
 
     static double collectionPower = 0;
 
-    static double ringPush = 0;
-    static double ringPushBack = 0;
+    static double ringPush = 0.8;
+    static double ringPushBack = 0.55;
 
     static double setShooterAngle = 0;
     static double shooterAngleIncrease = 0.03;
     static double shooterPower = 0;
 
-    static double elevatorTop = 0;
-    static double elevatorBottom= 0;
+    static double elevatorTop = 0.33;
+    static double elevatorBottom = 0.5;
 
-    static double topTilt = 0;
-    static double bottomTilt = 0;
+    static double topTilt = 0.76;
+    static double bottomTilt = 0.6;
 }
