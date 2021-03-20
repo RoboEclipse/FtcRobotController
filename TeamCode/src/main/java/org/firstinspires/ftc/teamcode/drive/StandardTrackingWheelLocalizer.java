89b96999f9e96e04bc7c1ffd71eac7ace2e110c1
erich <erichu100@outlook.com>
2021-03-19 19:18:56 -0700

    Added secondBotConstantStorage, fixed wheel radius in DriveConstants, changed lateral distance and forward offset to those of competition values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -35,8 +35,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 13.25; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.375; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -5; // in; offset of the lateral wheel
 
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
