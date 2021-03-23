ab3347108d61e698c3c7cde43d56de821804fc0b
erich <erichu100@outlook.com>
2021-03-22 21:13:31 -0700

    Autonomous Tuning -Removed a sleep at the start -Moved wobbleGoal trajectory generation. -Increased trackWidth -Tuned PID and lateral multiplier -Corrected lateralDistance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2   ; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14.5; // in
+    public static double TRACK_WIDTH = 15.3; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
