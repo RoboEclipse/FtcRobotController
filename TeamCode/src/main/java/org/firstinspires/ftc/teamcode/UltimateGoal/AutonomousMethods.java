35941f92827c9f71fae74f4b11612565712217d3
ericzhou16 <ericsuper@outlook.com>
2021-04-04 16:49:45 -0700

    Imported Pose2D in AutonomousMethods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/AutonomousMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/AutonomousMethods.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
