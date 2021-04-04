cbd2eda07c547fcb1fadbd073df0851308546e9e
erich <erichu100@outlook.com>
2021-04-04 15:03:46 -0700

    Autonomous Edits - Added AutoTransitioner - Disabled old program - Close claw during initialization - Tuned firstDropPositionMid and secondGrabPositionClose - Added raise wobble after dropping the first wobble goal and a sleep - Made it go back more afer dropping - Changed the secondDropPosition - Made lowerWobble() later for getSecondWobble - Got rid of unnecessary spline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/blueLeftAutonomousDetectionPreLoad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/blueLeftAutonomousDetectionPreLoad.java
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.UltimateGoal.AutonomousMethods;
 import org.firstinspires.ftc.teamcode.UltimateGoal.Constants;
@@ -13,6 +14,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Config
 @Autonomous(group = "drive")
+@Disabled
 public class blueLeftAutonomousDetectionPreLoad extends AutonomousMethods {
     private FtcDashboard dashboard;
     String detection;
